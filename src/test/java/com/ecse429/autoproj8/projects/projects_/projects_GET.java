68d644353b504b8109a356657a45925c214742e1
Matteo Barbieri <matteo.barbieri@mail.mcgill.ca>
2021-02-15 14:43:39 -0500

    Create projects_GET.java

---
+++ src/test/java/com/ecse429/autoproj8/projects/projects_/projects_GET.java
@@ -0,0 +1,55 @@
+package com.ecse429.autoproj8.projects.projects_;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Arrays;
+import java.util.List;
+
+import com.ecse429.autoproj8.models.Reference;
+import com.ecse429.autoproj8.models.Project;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Test;
+import static com.ecse429.autoproj8.ECSE429AutoProj8Tests.API_URI;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class projects_GET {
+
+    private static final String PROJECTS_URL = API_URI + "/projects";
+    
+    public static List<Project> projectsGetAll() throws IOException, InterruptedException {
+
+        var client = HttpClient.newHttpClient();
+    var request = HttpRequest.newBuilder().uri(URI.create(PROJECTS_URL)).GET().build();
+
+    var response = client.send(request, BodyHandlers.ofString());
+
+    assertEquals(response.statusCode(), 200);
+
+    var mapper = new ObjectMapper();
+    
+    var node = mapper.readTree(response.body());
+    Project[] arrayProject = mapper.readValue(node.path("projects").toString(), Project[].class);
+    return Arrays.asList(arrayProject);
+
+    }
+
+    @Test
+    public void projectsGetAllTest() throws IOException, InterruptedException {
+        List<Project> projects = projectsGetAll();
+
+        Project office = new Project(1, "Office Work", false, false, "", null, null);
+
+        System.out.println(office);
+        assertTrue(projects.contains(office));
+
+
+    }
+
+
+
+}
