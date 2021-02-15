6f43e81c073ad0b0060b8c71e44a5f7bee8ce1ae
Matt Langshur <matthewlangshur@gmail.com>
2021-02-15 13:58:38 -0500

    Finished all the categories test

--- src/test/java/com/ecse429/autoproj8/categories/categories_id_projects/Categories_id_projects__GET.java
+++ src/test/java/com/ecse429/autoproj8/categories/categories_id_projects/Categories_id_projects__GET.java
@@ -1,5 +1,62 @@
 package com.ecse429.autoproj8.categories.categories_id_projects;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import com.ecse429.autoproj8.models.Reference;
+import com.ecse429.autoproj8.models.Category;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Test;
+import static com.ecse429.autoproj8.ECSE429AutoProj8Tests.API_URI;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+// GET /todos
 public class Categories_id_projects__GET {
+
+    private static final int ID = 1;
+    private static final String CATEGORIES_URL = API_URI + "/categories/" + ID + "/projects";
+
+  public static List<Category> categoriesGetIDOne(int id) throws IOException, InterruptedException {
+    var client = HttpClient.newHttpClient();
+    var request = HttpRequest.newBuilder().uri(URI.create(CATEGORIES_URL)).GET().build();
+
+    var response = client.send(request, BodyHandlers.ofString());
+
+    assertEquals(response.statusCode(), 200);
+
+    var mapper = new ObjectMapper();
+
+    /**
+     * Special case, we need to use readTree first since Jackson can't parse when we have this:
+     * {
+     *    "todos": [{"id": "1", ...}, {}]
+     * }
+     * 
+     * It can only parse this: [{"id": "1", ...}, {}]
+     */
+    var node = mapper.readTree(response.body());
+    Category[] arrayCategory = mapper.readValue(node.path("categories").toString(), Category[].class);
+    return Arrays.asList(arrayCategory);
+  }
+
+  @Test
+  public void categoriesGetAllTest() throws IOException, InterruptedException {
+    List<Category> categories = categoriesGetIDOne(ID);
+
     
+
+    // Default created Todos
+    Category office = new Category(1, "Office", "", null, null);
+
+    System.out.println(office);
+    System.out.println(categories);
+    assertTrue(categories.contains(office));
+  }
 }
--- src/test/java/com/ecse429/autoproj8/categories/categories_id_projects_id/Categories_id_projects_id__DELETE.java
+++ src/test/java/com/ecse429/autoproj8/categories/categories_id_projects_id/Categories_id_projects_id__DELETE.java
@@ -1,5 +1,61 @@
 package com.ecse429.autoproj8.categories.categories_id_projects_id;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+
+import com.ecse429.autoproj8.models.Category;
+import com.ecse429.autoproj8.models.Reference;
+import com.ecse429.autoproj8.shutdown.Shutdown;
+import com.ecse429.autoproj8.categories.categories_.Categories__GET;
+import com.ecse429.autoproj8.categories.categories_id_projects.Categories_id_projects__POST;
+
+import org.junit.Test;
+import static com.ecse429.autoproj8.ECSE429AutoProj8Tests.API_URI;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+// DELETE /categories/:id
 public class Categories_id_projects_id__DELETE {
+
+  private static final int ID = 1;
+  private static final int ID2 = 2;
+
+  private static final String CATEGORIES_URL = API_URI + "/categories/" + ID + "/projects/" + ID2;
+
+  public static void deleteCategory(int id, int id2) throws IOException, InterruptedException {
+    var client = HttpClient.newHttpClient();
+  
+    var request = HttpRequest.newBuilder().uri(URI.create(CATEGORIES_URL)).DELETE().build();
+
+    var response = client.send(request, BodyHandlers.ofString());
+    assertEquals(response.statusCode(), 200);
+  }
+
+  @Test
+  public void categoriesDeleteIDTest() throws IOException, InterruptedException {
     
+    Reference ref = new Reference(1);
+    String[] exclude = {"id", "categories", "projects", "todos"};
+
+    Categories_id_projects__POST.categoriesCreateCategory(ref, exclude);
+    List<Category> cat = Categories__GET.categoriesGetAll();
+    System.out.println(cat);
+
+    deleteCategory(ID, ID2);
+
+    System.out.println(ref);
+    System.out.println(cat);
+    assertFalse(cat.contains(ref));
+
+    // restart server
+    Shutdown.shutdown();
+    Runtime rt = Runtime.getRuntime();
+    rt.exec("java -jar runTodoManagerRestAPI-1.5.5.jar");
+  }
 }
